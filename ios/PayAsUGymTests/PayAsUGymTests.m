9de286bdb89deef84050dd745958c0ae2bcf7e41
“Maksym <maksym.popov@litslink.com>
2018-11-12 13:43:01 +0200

    Init

--- ios/PayAsUGymTests/PayAsUGymTests.m
+++ ios/PayAsUGymTests/PayAsUGymTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface cvcTests : XCTestCase
+@interface PayAsUGymTests : XCTestCase
 
 @end
 
-@implementation cvcTests
+@implementation PayAsUGymTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
--- src/navigation/config.tsx
+++ src/navigation/config.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 interface Route {
   [key: string]: string;
 }
