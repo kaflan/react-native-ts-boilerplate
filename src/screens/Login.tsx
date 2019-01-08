44ebbaf54dda2cd7dbdd5dbc7d5705aa5b8204c7
AndreyKatrusha <andrey.katrusha@litslink.com>
2019-01-08 13:23:44 +0200

    Add StyleSheet Add lock yarn init repo

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  }
+});
 
 export default class Login extends Component<void> {
   render() {
     return (
-      <View>
-        <Text>Login screen</Text>
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Login screen</Text>
       </View>
     );
   }
