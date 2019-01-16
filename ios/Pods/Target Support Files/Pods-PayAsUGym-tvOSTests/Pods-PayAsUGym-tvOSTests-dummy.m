f502cfafed2d05cbe1164cc2b5c1623d8b5b7ad6
AndreyKatrusha <andrey.katrusha@litslink.com>
2019-01-16 15:06:40 +0200

    fix: ios linked

---
+++ ios/Pods/Target Support Files/Pods-PayAsUGym-tvOSTests/Pods-PayAsUGym-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PayAsUGym_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_PayAsUGym_tvOSTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-PayAsUGym/Pods-PayAsUGym-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PayAsUGym : NSObject
+@end
+@implementation PodsDummy_Pods_PayAsUGym
+@end
