c3fb64b7e9dbeebdacfa981333b2695cf426f469
AndreyKatrusha <andrey.katrusha@litslink.com>
2019-01-16 16:20:57 +0200

    Remove ignored files

--- ios/Pods/Target Support Files/Pods-PayAsUGym-tvOSTests/Pods-PayAsUGym-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PayAsUGym_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_PayAsUGym_tvOSTests
-@end
--- ios/Pods/Target Support Files/Pods-PayAsUGym/Pods-PayAsUGym-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PayAsUGym : NSObject
-@end
-@implementation PodsDummy_Pods_PayAsUGym
-@end
