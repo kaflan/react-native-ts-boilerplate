251b6cbc50d58c4078409d2c08b9356528a62ff2
AndreyKatrusha <andrey.katrusha@litslink.com>
2019-01-17 14:17:14 +0200

    Add android bundle

--- android/app/src/main/java/com/payasugym/MainActivity.java
+++ android/app/src/main/java/com/payasugym/MainActivity.java
@@ -1,9 +1,22 @@
 package com.payasugym;
 
 import com.facebook.react.ReactActivity;
+import android.content.Intent;     // <--- import
+import android.os.Bundle;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 
 public class MainActivity extends ReactActivity {
 
+    CallbackManager mCallbackManager;
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
--- android/app/src/main/java/com/payasugym/MainApplication.java
+++ android/app/src/main/java/com/payasugym/MainApplication.java
@@ -3,16 +3,26 @@ package com.payasugym;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.facebook.react.ReactActivity;
+import android.content.Intent;     // <--- import
+import android.os.Bundle;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+  CallbackManager mCallbackManager;
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -20,13 +30,15 @@ public class MainApplication extends Application implements ReactApplication {
       return BuildConfig.DEBUG;
     }
 
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNFirebasePackage()
-      );
-    }
+      @Override
+      protected List<ReactPackage> getPackages() {
+          mCallbackManager = new CallbackManager.Factory().create();
+          ReactPackage packages[] = new ReactPackage[]{
+                  new MainReactPackage(),
+                  new FBSDKPackage(mCallbackManager),
+          };
+          return Arrays.<ReactPackage>asList(packages);
+      }
 
     @Override
     protected String getJSMainModuleName() {
