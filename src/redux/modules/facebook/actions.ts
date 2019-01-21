b58b191dad42ab38bff2d29b470825d932d873b5
AndreyKatrusha <andrey.katrusha@litslink.com>
2019-01-21 16:25:41 +0200

    add: film modules, facebook modules

---
+++ src/redux/modules/facebook/actions.ts
@@ -0,0 +1,13 @@
+import actionCreatorFactory from 'typescript-fsa';
+import { IFilm } from './types'
+
+const actionCreator = actionCreatorFactory('@@facebook');
+
+export const start = actionCreator<void>('START');
+
+export const initialized = actionCreator<void>('INITIALIZED');
+
+export const finish = actionCreator<void>('FINISH');
+
+export const filmListLoaded = actionCreator<IFilm[]>('FINISH_LOADED');
+
---
+++ src/redux/modules/films/saga.ts
@@ -0,0 +1,23 @@
+import { SagaIterator } from 'redux-saga';
+import firebase from 'react-native-firebase'
+// import { call, takeEvery, put } from 'redux-saga/effects';
+// import { start, filmListLoaded } from './actions';
+
+
+
+// Calling the following function will open the FB login dialogue:
+const facebookLogin = async () => {
+  try {
+
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+function* initWorker(): SagaIterator {
+
+}
+
+export default function* root(): SagaIterator {
+
+}
