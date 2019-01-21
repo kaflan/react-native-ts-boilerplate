afc75bb23625f84e346f5c5fab9bfd83dfaf5cef
AndreyKatrusha <andrey.katrusha@litslink.com>
2019-01-21 16:26:59 +0200

    coment modules

--- src/redux/modules/films/reducer.ts
+++ src/redux/modules/films/reducer.ts
@@ -1,29 +1,29 @@
-import { reducerWithInitialState } from 'typescript-fsa-reducers';
-import { initialized, filmListLoaded } from './actions';
-
-
-
-interface RootReducer extends IState {
-  filmList?: IFilm[]
-}
-
-const INITIAL_STATE: RootReducer = {
-  isInitialized: false,
-  filmList: []
-};
-const reducer = reducerWithInitialState<RootReducer>(INITIAL_STATE)
-  .case(
-    initialized,
-    (state) => ({
-      ...state,
-      isInitialized: true,
-    })
-  )
-  .case(filmListLoaded, (state, amount) => {
-    return ({
-      ...state,
-      filmList: amount,
-    })
-  });
-
-export { reducer as global };
+// import { reducerWithInitialState } from 'typescript-fsa-reducers';
+// import { initialized, filmListLoaded } from './actions';
+//
+//
+//
+// interface RootReducer extends IState {
+//   filmList?: IFilm[]
+// }
+//
+// const INITIAL_STATE: RootReducer = {
+//   isInitialized: false,
+//   filmList: []
+// };
+// const reducer = reducerWithInitialState<RootReducer>(INITIAL_STATE)
+//   .case(
+//     initialized,
+//     (state) => ({
+//       ...state,
+//       isInitialized: true,
+//     })
+//   )
+//   .case(filmListLoaded, (state, amount) => {
+//     return ({
+//       ...state,
+//       filmList: amount,
+//     })
+//   });
+//
+// export { reducer as global };
